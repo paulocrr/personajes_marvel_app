2ccb9c5d59421cba242a84e0b8912881fbc3f5ee
Paulo Rodriguez <paulo_crr@hotmail.com>
2025-02-22 11:25:33 -0500

    feat: Se implento paginacion

--- lib/screens/pantalla_lista_super_heroes.dart
+++ lib/screens/pantalla_lista_super_heroes.dart
@@ -1,9 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:personajes_marvel_app/core/failures/failures.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:personajes_marvel_app/models/super_heroe.dart';
 import 'package:personajes_marvel_app/repositories/super_heroes_repository.dart';
-import 'package:personajes_marvel_app/ui/mostrar_falla.dart';
 
 class PantallaListaSuperHeroes extends StatefulWidget {
   const PantallaListaSuperHeroes({super.key});
@@ -14,71 +13,54 @@ class PantallaListaSuperHeroes extends StatefulWidget {
 }
 
 class _PantallaListaSuperHeroesState extends State<PantallaListaSuperHeroes> {
-  List<SuperHeroe>? _listaSuperHeroes;
-  Failure? _falla;
   final superHeroesRespository = SuperHeroesRepository();
   var estaCargando = false;
+  final _pagingController = PagingController<int, SuperHeroe>(firstPageKey: 1);
 
   @override
   void initState() {
-    _obtenerSuperHeroes();
+    _pagingController.addPageRequestListener((pageKey) {
+      _obtenerSuperHeroes(pageKey);
+    });
     super.initState();
   }
 
-  void _obtenerSuperHeroes() async {
-    setState(() {
-      _listaSuperHeroes = null;
-      _falla = null;
-      estaCargando = true;
-    });
+  void _obtenerSuperHeroes(int pageKey) async {
+    final limit = 15;
+    final offset = (pageKey - 1) * limit;
     final superHeroesResultado = await superHeroesRespository
-        .obtenerSuperHeroes(limit: 100);
-    setState(() {
-      superHeroesResultado.match(
-        (falla) {
-          _falla = falla;
-        },
-        (data) {
-          _listaSuperHeroes = data.results;
-        },
-      );
-
-      estaCargando = false;
+        .obtenerSuperHeroes(limit: limit, offset: offset);
+    superHeroesResultado.fold((falla) {
+      _pagingController.error = falla;
+    }, (listaSuperHeroes) {
+      final esPaginaFinal = listaSuperHeroes.results.length < limit;
+      if (esPaginaFinal) {
+        _pagingController.appendLastPage(listaSuperHeroes.results);
+      } else {
+        _pagingController.appendPage(listaSuperHeroes.results, pageKey + 1);
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    if (estaCargando == true) {
-      return Scaffold(
-        appBar: AppBar(title: Text('Lista de Super Heroes')),
-        body: Center(child: SpinKitSquareCircle(color: Colors.red, size: 50.0)),
-      );
-    }
-
-    if (_falla != null) {
-      return MostrarFalla(
-        falla: _falla!,
-        reintentar: () {
-          _obtenerSuperHeroes();
-        },
-      );
-    }
-
     return Scaffold(
       appBar: AppBar(title: Text('Lista de Super Heroes')),
-      body: ListView.builder(
-        itemCount: _listaSuperHeroes!.length,
-        itemBuilder: (_, indice) {
-          final superHeroe = _listaSuperHeroes![indice];
-          final numeroDeOrden = indice + 1;
-          return ListTile(
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(superHeroe.thumbnail.rutaCompleta),
-            ),
-            title: Text('#$numeroDeOrden ${superHeroe.name}'),
-          );
-        },
+      body: PagedListView<int, SuperHeroe>(
+        pagingController: _pagingController,
+        builderDelegate: PagedChildBuilderDelegate(
+          animateTransitions: true,
+          itemBuilder: (_, superHeroe, indice) {
+            final numeroDeOrden = indice + 1;
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: CachedNetworkImageProvider(
+                    superHeroe.thumbnail.rutaCompleta),
+              ),
+              title: Text('#$numeroDeOrden ${superHeroe.name}'),
+            );
+          },
+        ),
       ),
     );
   }
